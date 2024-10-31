9de937d6e6f99014de56c969a2ccaf572e4f54ba
pritom <chakmapritom1@gmail.com>
2024-10-31 21:51:10 +0600

    set the nav section and banner section

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Home from "./Components/Home/Home";
 import Root from "./Components/Root/Root";
 import "./index.css";
 
@@ -8,6 +11,17 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+    ],
   },
 ]);
 
